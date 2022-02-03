0d6a770c57728b61bb9b76641c5cf675a3a2c1ee
Neotoxic-off <neotoxic.off@gmail.com>
2022-02-03 14:52:55 +0100

    init commit

---
+++ CR34/CR34/CR34.Designer.cs
@@ -0,0 +1,272 @@
+﻿
+namespace CR34
+{
+    partial class CR34
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CR34));
+            this.border = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_reduce = new System.Windows.Forms.Button();
+            this.button_close = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tag = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pages = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.logs = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.border.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pages)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // border
+            // 
+            this.border.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(26)))), ((int)(((byte)(29)))));
+            this.border.Controls.Add(this.button2);
+            this.border.Controls.Add(this.label1);
+            this.border.Controls.Add(this.button_reduce);
+            this.border.Controls.Add(this.button_close);
+            this.border.Dock = System.Windows.Forms.DockStyle.Top;
+            this.border.Location = new System.Drawing.Point(0, 0);
+            this.border.Name = "border";
+            this.border.Size = new System.Drawing.Size(374, 25);
+            this.border.TabIndex = 2;
+            this.border.MouseDown += new System.Windows.Forms.MouseEventHandler(this.border_MouseDown);
+            this.border.MouseMove += new System.Windows.Forms.MouseEventHandler(this.border_MouseMove);
+            this.border.MouseUp += new System.Windows.Forms.MouseEventHandler(this.border_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(3, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "v1.0.0";
+            // 
+            // button_reduce
+            // 
+            this.button_reduce.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(26)))), ((int)(((byte)(29)))));
+            this.button_reduce.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button_reduce.BackgroundImage")));
+            this.button_reduce.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button_reduce.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button_reduce.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(26)))), ((int)(((byte)(29)))));
+            this.button_reduce.FlatAppearance.BorderSize = 0;
+            this.button_reduce.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(26)))), ((int)(((byte)(29)))));
+            this.button_reduce.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(26)))), ((int)(((byte)(29)))));
+            this.button_reduce.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_reduce.ForeColor = System.Drawing.Color.Transparent;
+            this.button_reduce.Location = new System.Drawing.Point(324, 0);
+            this.button_reduce.Name = "button_reduce";
+            this.button_reduce.Size = new System.Drawing.Size(25, 25);
+            this.button_reduce.TabIndex = 3;
+            this.button_reduce.UseVisualStyleBackColor = false;
+            this.button_reduce.Click += new System.EventHandler(this.button_reduce_Click);
+            // 
+            // button_close
+            // 
+            this.button_close.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(66)))), ((int)(((byte)(69)))));
+            this.button_close.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button_close.BackgroundImage")));
+            this.button_close.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button_close.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button_close.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(66)))), ((int)(((byte)(69)))));
+            this.button_close.FlatAppearance.BorderSize = 0;
+            this.button_close.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(66)))), ((int)(((byte)(69)))));
+            this.button_close.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(66)))), ((int)(((byte)(69)))));
+            this.button_close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_close.ForeColor = System.Drawing.Color.Transparent;
+            this.button_close.Location = new System.Drawing.Point(349, 0);
+            this.button_close.Name = "button_close";
+            this.button_close.Size = new System.Drawing.Size(25, 25);
+            this.button_close.TabIndex = 2;
+            this.button_close.UseVisualStyleBackColor = false;
+            this.button_close.Click += new System.EventHandler(this.button_close_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(350, 210);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tag
+            // 
+            this.tag.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(229)))), ((int)(((byte)(163)))));
+            this.tag.ForeColor = System.Drawing.Color.White;
+            this.tag.Location = new System.Drawing.Point(61, 19);
+            this.tag.Name = "tag";
+            this.tag.Size = new System.Drawing.Size(283, 20);
+            this.tag.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Tag:";
+            // 
+            // pages
+            // 
+            this.pages.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(229)))), ((int)(((byte)(163)))));
+            this.pages.ForeColor = System.Drawing.Color.White;
+            this.pages.Location = new System.Drawing.Point(62, 45);
+            this.pages.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.pages.Name = "pages";
+            this.pages.Size = new System.Drawing.Size(282, 20);
+            this.pages.TabIndex = 6;
+            this.pages.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(229)))), ((int)(((byte)(163)))));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(269, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.pages);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.tag);
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(12, 266);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(350, 103);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = " Search ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Pages:";
+            // 
+            // logs
+            // 
+            this.logs.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(229)))), ((int)(((byte)(163)))));
+            this.logs.Location = new System.Drawing.Point(12, 375);
+            this.logs.Name = "logs";
+            this.logs.ReadOnly = true;
+            this.logs.Size = new System.Drawing.Size(350, 124);
+            this.logs.TabIndex = 9;
+            this.logs.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(26)))), ((int)(((byte)(29)))));
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(26)))), ((int)(((byte)(29)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(26)))), ((int)(((byte)(29)))));
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(26)))), ((int)(((byte)(29)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.Transparent;
+            this.button2.Location = new System.Drawing.Point(299, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(25, 25);
+            this.button2.TabIndex = 4;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // CR34
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(229)))), ((int)(((byte)(163)))));
+            this.ClientSize = new System.Drawing.Size(374, 511);
+            this.Controls.Add(this.logs);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.border);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "CR34";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CR34";
+            this.Load += new System.EventHandler(this.CR34_Load);
+            this.border.ResumeLayout(false);
+            this.border.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pages)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel border;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_reduce;
+        private System.Windows.Forms.Button button_close;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox tag;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown pages;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox logs;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ CR34/CR34/CR34.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+using ComponentFactory.Krypton.Toolkit;
+using System.Xml;
+using RestSharp;
+using System.Net;
+
+namespace CR34
+{
+    public partial class CR34 : KryptonForm
+    {
+        bool mouse_down = false;
+        private Point offset;
+
+        private BackgroundWorker worker = new BackgroundWorker();
+        private string url = $"https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&tags=";
+
+        public CR34()
+        {
+            InitializeComponent();
+            InitializeWorker();
+        }
+
+        private void InitializeWorker()
+        {
+            worker.DoWork += new DoWorkEventHandler(download);
+        }
+
+        private void button_close_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button_reduce_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void CR34_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            pages.Enabled = false;
+            tag.Enabled = false;
+
+            worker.RunWorkerAsync();
+            while (worker.IsBusy)
+            {
+                Application.DoEvents();
+            }
+
+            button1.Enabled = true;
+            pages.Enabled = true;
+            tag.Enabled = true;
+        }
+
+        private async Task<string> cleanner(string downloads, string data)
+        {
+            return ($"{downloads}\\{data.Replace(" ", "_")}");
+        }
+
+        private async void download(object sender, EventArgs e)
+        {
+            int aligner = 42;
+            WebClient client = new WebClient();
+            string result = null;
+            int pages_number = Convert.ToInt32(pages.Value);
+            string full_url = null;
+            XmlDocument post = new XmlDocument();
+            XmlNodeList node = null;
+            string downloads = "downloads";
+            string output_path = await cleanner(downloads, tag.Text);
+            string output_name = null;
+            string address = null;
+
+            if (Directory.Exists(downloads) == false)
+                Directory.CreateDirectory(downloads);
+            if (Directory.Exists(output_path) == false)
+                Directory.CreateDirectory(output_path);
+
+            for (int pid = 0; pid < pages_number; pid++)
+            {
+                full_url = $"{url}{tag.Text}+&pid={pid * aligner}";
+                await update_logs(full_url);
+                result = client.DownloadString(full_url);
+                post.LoadXml(result);
+                node = post.SelectNodes("posts");
+
+                for (int i = 0; i < node.Count; i++)
+                {
+                    for (int child = 0; child < node[i].ChildNodes.Count; child++)
+                    {
+                        address = node[i].ChildNodes[child].Attributes[i].OwnerElement.GetAttribute("file_url");
+                        output_name = address.Split('/')[5];
+                        if (File.Exists($"{output_path}\\{output_name}") == false)
+                        {
+                            await update_logs(output_name);
+                            client.DownloadFile(address, $"{output_path}\\{output_name}");
+                        }
+                    }
+                }
+            }
+        }
+
+        private async Task<Task> update_logs(string data)
+        {
+            logs.Invoke(new MethodInvoker(async delegate
+            {
+                logs.AppendText($"{data}\n");
+            }));
+
+            return (Task.CompletedTask);
+        }
+
+        private void border_MouseDown(object sender, MouseEventArgs e)
+        {
+            offset.X = e.X;
+            offset.Y = e.Y;
+
+            mouse_down = true;
+        }
+
+        private void border_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouse_down == true)
+            {
+                Point pos = PointToScreen(e.Location);
+                Location = new Point(pos.X - offset.X, pos.Y - offset.Y);
+            }
+        }
+
+        private void border_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouse_down = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Author: Neo\nGithub: https://github.com/Neotoxic-off", "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
